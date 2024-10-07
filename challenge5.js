b0cc08dd4d1d915835257980849c1d8d8d7767ff
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-07 11:29:25 +0200

    Create doubling function

--- challenge5.js
+++ challenge5.js
@@ -1,5 +1,9 @@
-const numbers = Object.freeze([3, 14, 15, 92 ,65, 35, 89, 79, 32, 38]);
+const numbers = Object.freeze([3, 14, 15, 92, 65, 35, 89, 79, 32, 38]);
 
-const number1 = (numbers)=> console.log(numbers);
+const n1 = (numbers) => console.log(numbers);
 
-number1(numbers)
+//number1(numbers)
+
+const n2 = numbers.map((x) => x * 2);
+
+console.log(n2);
