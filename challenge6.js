2381265ab3346399ae5fd1b0bf77f39de42296f5
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-08 16:26:08 +0200

    Create poll

--- challenge6.js
+++ challenge6.js
@@ -1,4 +1,12 @@
 const examplePrompt = {
-    question: "What is your favorite programming language?",
-    options: ["1. JavaScript", "2. PHP", "3. Java", "4. Assembly"]
-  };
+  question: "What is your favorite programming language?",
+  options: ["1. JavaScript", "2. PHP", "3. Java", "4. Assembly"],
+};
+
+const createPoll = (prompt) => {
+  console.log(prompt.question);
+  prompt.options.forEach((el) => {console.log(el)});
+  
+};
+
+createPoll(examplePrompt);
