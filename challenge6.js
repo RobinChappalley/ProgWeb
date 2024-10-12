da28fe76552a2113a33ec5e76a5070fb3fa16d7d
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-12 12:44:06 +0200

    Tried updating vote function, lines 25-30 don't work

--- challenge6.js
+++ challenge6.js
@@ -17,17 +17,23 @@ function createPoll(prompt) {
     question: prompt.question,
     optionsAndVotes,
     vote: (optionChoosed) => {
+      const index = optionChoosed;
+      console.log(optionChoosed)
       if (typeof optionChoosed !== "number") {
         console.error("You can't vote for this option");
       } else {
-        this.optionsAndVotes.set(vote, this.optionsAndVotes.get(vote) + 1);
+        const optionChoosed = this.optionsAndVotes.get(index);
+        console.log(optionChoosed);
+        optionsAndVotes.set(
+          optionChoosed,
+          optionsAndVotes.get(optionsAndVotes[index] + 1)
+        );
       }
       //console.log(`You voted for ${poll.options[vote - 1]}`);
       //displays(); // displays the updated poll
     },
   };
 }
-
 
 const poll = createPoll(examplePrompt);
 const poll2 = createPoll(coursePrompt);
