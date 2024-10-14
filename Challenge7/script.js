11f7a071168492e7dccfb7ea649501f464c2705f
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-14 11:09:17 +0200

    Check number validity

--- Challenge7/script.js
+++ Challenge7/script.js
@@ -3,7 +3,15 @@
 const randomNumber = () => {
   return Math.floor(Math.random() * 20) + 1;
 };
-
+const secretNumber = randomNumber();
+console.log(secretNumber);
 document.querySelector("#check").addEventListener("click", () => {
   document.querySelector("#score").textContent--;
+  if (checkNumber()) {
+    
+  }
 });
+
+const checkNumber = () => {
+  return document.querySelector("#guess").value == secretNumber;
+};
