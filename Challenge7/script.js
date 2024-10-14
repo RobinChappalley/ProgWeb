ade45935fd43c78c592109cd3e7fc2bd8b9b0578
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-14 10:59:03 +0200

    Create decremental function when click

--- Challenge7/script.js
+++ Challenge7/script.js
@@ -3,3 +3,7 @@
 const randomNumber = () => {
   return Math.floor(Math.random() * 20) + 1;
 };
+
+document.querySelector("#check").addEventListener("click", () => {
+  document.querySelector("#score").textContent--;
+});
