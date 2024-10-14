df2730d284cdccb0d701f4cfefce6f21f44820c2
RobinChappalley <165492219+RobinChappalley@users.noreply.github.com>
2024-10-14 11:19:00 +0200

    Create validity check of input

--- Challenge7/script.js
+++ Challenge7/script.js
@@ -7,11 +7,23 @@ const secretNumber = randomNumber();
 console.log(secretNumber);
 document.querySelector("#check").addEventListener("click", () => {
   document.querySelector("#score").textContent--;
-  if (checkNumber()) {
-    
+
+  if (checkValidity()) {
+    if (checkNumber()) {
+    }
   }
 });
 
 const checkNumber = () => {
   return document.querySelector("#guess").value == secretNumber;
+};
+const checkValidity = () => {
+  if (
+    !document.querySelector("#guess").value ||
+    document.querySelector("#guess").value < 1 ||
+    document.querySelector("#guess").value > 20
+  ) {
+    document.querySelector("#hint").textContent =
+      "Guess must be between 1 and 20 !";
+  }
 };
