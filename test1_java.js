d0dee753e030e193862483ae167c2fcaa6993218
RobinChappalley <robinchappalley@gmail.com>
2024-10-21 15:16:48 +0200

    Create mocing cases function

--- test1_java.js
+++ test1_java.js
@@ -1,12 +1,30 @@
 // console.log("Quel est ton nom, intrépide aventurier ?")
 // const name = readlineSync.question("Quel est ton nom, intrépide aventurier ?")
-const nbcases = 25;
-const plateau = createArrayWithNumbersFromRange(1,25);
-
-
-// const userInput = prompt("Entrez votre prénom :");
-// prompt("Vous avez entré : " + userInput);
-
-function createArrayWithNumbersFromRange(start, end) {
+const createArrayWithNumbersFromRange = (start, end) => {
   return [...Array(end - start + 1)].map((_, index) => start + index);
-}
+};
+const nbcases = 25;
+const plateau = createArrayWithNumbersFromRange(1, 25);
+let caseActuelle = 1;
+
+const userInput = prompt("Entrez votre prénom :");
+prompt(`Bienvenue ${userInput}, c'est à toi de jouer !
+    Lance ton dé et indique moi sa valeur`);
+
+const jeu = () => {
+  let win = false;
+  do {} while (!win);
+};
+
+const movingCases = () => {
+  const move = new Map();
+  up.set(1, 9);
+  up.set(4, 13);
+  up.set(20, 22);
+  up.set(8, 2);
+  up.set(6, 5);
+  up.set(19, 10);
+  up.set(17, 7);
+  up.set(24, 18);
+  return move;
+};
