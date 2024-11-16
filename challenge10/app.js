69530054b302e7aff4c20610e22c7b37ebb42772
RobinChappalley <robinchappalley@gmail.com>
2024-11-16 20:12:25 +0100

    Create variables

--- challenge10/app.js
+++ challenge10/app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+
+const weight = document.querySelector("input[name=weight]");
+const size = document.querySelector("input[name=size]");
+const leading = document.querySelector("input[name=leading]");
+
+console.log(size) 
+console.log(weight)
+console.log(leading) 
