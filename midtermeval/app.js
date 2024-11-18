e8b639f84b0d7d6ba4e454d3b9699d1e0a55d753
RobinChappalley <robinchappalley@gmail.com>
2024-11-18 10:40:47 +0100

    Create toggle for tasks

--- midtermeval/app.js
+++ midtermeval/app.js
@@ -2,21 +2,19 @@
 
 const input = document.querySelector("#input-todo");
 const list = document.querySelector("#list");
-const categories = document.querySelector("#category-container")
+const categories = document.querySelector("#category-container");
 let taskname = "";
-
 
 // console.log(input)
 
-
 const taskCreator = (newtaskname) => {
-  const htmlTask = `<li data-category="fun" data-done="false">
+  const htmlTask = `<li data-category="${newtaskname}" data-done="false">
     ${newtaskname}
     <div class="button-done">❌</div>
   </li> `;
   list.insertAdjacentHTML("afterbegin", htmlTask);
-  input.blur()
-  input.value = ''
+  input.blur();
+  input.value = "";
 };
 
 input.addEventListener("keyup", (e) => {
@@ -26,25 +24,27 @@ input.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     taskCreator(input.value);
   }
-  
 });
 
-categories.addEventListener("click" , (e) => {
-    //console.log(e.target.dataset)
-    if (e.target !== e.currentTarget) {
-        //console.log(e.target)
-        //e.currentTarget.dataset.selected = false
-       // e.target.dataset.selected = true
-
-        changeSelection(e)
-    }
-
-})
+categories.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) {
+    changeSelection(e);
+  }
+});
 
 const changeSelection = (e) => {
-    if (e.target.dataset.selected){
-        e.target.dataset.selected = true
-        
-    }
+  if (e.target.dataset.selected) {
+    e.target.dataset.selected = true;
+  }
+};
 
-}
+list.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) {
+    //toggle to switch state for tasks
+    if (e.target.dataset.done === "true") {
+      e.target.dataset.done = false;
+    } else if (e.target.dataset.done === "false") {
+      e.target.dataset.done = true;
+    }
+  }
+});
