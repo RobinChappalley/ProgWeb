969d415fb8b66950f48bd83a6cc95b836f92e408
RobinChappalley <robinchappalley@gmail.com>
2024-11-18 10:13:03 +0100

    remove focus when a task is added

--- midtermeval/app.js
+++ midtermeval/app.js
@@ -2,10 +2,12 @@
 
 const input = document.querySelector("#input-todo");
 const list = document.querySelector("#list");
+const categories = document.querySelector("#category-container")
 let taskname = "";
 
+
 // console.log(input)
-console.log(list);
+
 
 const taskCreator = (newtaskname) => {
   const htmlTask = `<li data-category="fun" data-done="false">
@@ -13,6 +15,8 @@ const taskCreator = (newtaskname) => {
     <div class="button-done">❌</div>
   </li> `;
   list.insertAdjacentHTML("afterbegin", htmlTask);
+  input.blur()
+  input.value = ''
 };
 
 input.addEventListener("keyup", (e) => {
@@ -22,4 +26,25 @@ input.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     taskCreator(input.value);
   }
+  
 });
+
+categories.addEventListener("click" , (e) => {
+    //console.log(e.target.dataset)
+    if (e.target !== e.currentTarget) {
+        //console.log(e.target)
+        //e.currentTarget.dataset.selected = false
+       // e.target.dataset.selected = true
+
+        changeSelection(e)
+    }
+
+})
+
+const changeSelection = (e) => {
+    if (e.target.dataset.selected){
+        e.target.dataset.selected = true
+        
+    }
+
+}
