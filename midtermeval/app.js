f9e143ad92ec304e44ff425d50fa9b4e417bde08
RobinChappalley <robinchappalley@gmail.com>
2024-11-18 09:40:53 +0100

    Insert task text

--- midtermeval/app.js
+++ midtermeval/app.js
@@ -2,20 +2,24 @@
 
 const input = document.querySelector("#input-todo");
 const list = document.querySelector("#list");
+let taskname = "";
 
 // console.log(input)
 console.log(list);
 
-const taskCreator = (e) => {
-  console.log(e.key);
-  const task = {
-    text: input,
-  };
+const taskCreator = (newtaskname) => {
+  const htmlTask = `<li data-category="fun" data-done="false">
+    ${newtaskname}
+    <div class="button-done">❌</div>
+  </li> `;
+  list.insertAdjacentHTML("afterbegin", htmlTask);
 };
 
 input.addEventListener("keyup", (e) => {
-  taskCreator(e);
+  input.addEventListener("input", () => {
+    return input.value;
+  });
+  if (e.key === "Enter") {
+    taskCreator(input.value);
+  }
 });
-
-
-
