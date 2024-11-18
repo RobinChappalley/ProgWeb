c3f559d36dac0e90c8c0ae712a8a60399871e6fe
RobinChappalley <robinchappalley@gmail.com>
2024-11-18 11:54:05 +0100

    Add comments

--- midtermeval/app.js
+++ midtermeval/app.js
@@ -8,6 +8,8 @@ let chosenCategory = null;
 let taskname = "";
 
 const taskCreator = (newtaskname) => {
+    //create the new element and add it
+    //then remove focus and empties input field
   const htmlTask = `<li data-category="${chosenCategory}" data-done="false">
     ${newtaskname}
     <div class="button-done">❌</div>
@@ -15,10 +17,10 @@ const taskCreator = (newtaskname) => {
   list.insertAdjacentHTML("afterbegin", htmlTask);
   input.blur();
   input.value = "";
-  //console.log(eraseButtons)
 };
 
 const changeSelection = (e) => {
+    //switches categories to accept only one possiblity
   if (e.target.dataset.selected === "false") {
     for (const children of categories.children) {
       children.dataset.selected = false;
@@ -29,7 +31,9 @@ const changeSelection = (e) => {
     e.target.dataset.selected = false;
   }
 };
+
 input.addEventListener("keyup", (e) => {
+    //gets input and creates a task
   input.addEventListener("input", () => {
     return input.value;
   });
@@ -39,6 +43,8 @@ input.addEventListener("keyup", (e) => {
 });
 
 categories.addEventListener("click", (e) => {
+    // ensure that nothing happens when you click between categories
+    //
   if (e.target !== e.currentTarget) {
     changeSelection(e);
   }
@@ -54,6 +60,7 @@ list.addEventListener("click", (e) => {
     }
   }
 
+  //remove tasks
   if (e.target.classList[0] === "button-done") {
     e.target.parentElement.parentElement.removeChild(e.target.parentElement);
   }
