f71b5fc56dc625d981e1d8f39468dd966723e204
RobinChappalley <robinchappalley@gmail.com>
2024-11-18 09:17:40 +0100

    Get keys

--- midtermeval/app.js
+++ midtermeval/app.js
@@ -1,1 +1,21 @@
-"use strict"
+"use strict";
+
+const input = document.querySelector("#input-todo");
+const list = document.querySelector("#list");
+
+// console.log(input)
+console.log(list);
+
+const taskCreator = (e) => {
+  console.log(e.key);
+  const task = {
+    text: input,
+  };
+};
+
+input.addEventListener("keyup", (e) => {
+  taskCreator(e);
+});
+
+
+
