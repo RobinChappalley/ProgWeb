71cf0ab59c22549354a0f1a4b16f6c21dac06f2a
RobinChappalley <robinchappalley@gmail.com>
2024-11-25 12:42:22 +0100

    add css to adapt inputs

--- Challenge11/app.js
+++ Challenge11/app.js
@@ -1,21 +1,21 @@
 "use strict";
 const request = new XMLHttpRequest();
 const container = document.querySelector("#board");
-const input = document.querySelector("#input-station");
+const stationName = document.querySelector("h1");
 
 // Add input field for selecting a new station
 const addInput = () => {
   const label = document.createElement("label");
   label.textContent = "Nouvelle station :";
   container.insertAdjacentElement("beforebegin", label);
-
+//   container.insertAdjacentHTML("beforebegin", "<br></br>");
   const inputfield = document.createElement("input");
   inputfield.id = "input-station";
   inputfield.type = "text";
   inputfield.placeholder = "Select a new station";
 
   container.insertAdjacentElement("beforebegin", inputfield);
-
+  container.insertAdjacentHTML("beforebegin", "<br></br>");
   const button = document.createElement("button");
   button.textContent = "Prendre le train";
   container.insertAdjacentElement("beforebegin", button);
@@ -23,28 +23,26 @@ const addInput = () => {
 
 addInput();
 const button = document.querySelector("button");
+const input = document.querySelector("#input-station");
 const getStation = (input) => {
-  const station = input ? input.value : "Yverdon-les-bains";
+  const station = input ? input : "Yverdon-les-bains";
   request.open(
     "GET",
     `http://transport.opendata.ch/v1/stationboard?station=${station}&limit=5`
   );
 
-  console.log(station);
   request.send();
   request.addEventListener("load", () => {
     const response = JSON.parse(request.response);
     print(response.stationboard);
+    stationName.textContent = response.station.name;
   });
 };
 
 getStation();
 const print = (train) => {
-  //container.innerHTML = ""; // Clear previous results
-  // if (train.length === 0) {
-  //   container.textContent = "Aucun train disponible pour cette station";
-  //   return;
-  // }
+  container.innerHTML = ""; // Clear previous results
+
   train.forEach((t) => {
     const departureTime = new Date(t.stop.departure);
     const hours = departureTime.getHours().toString().padStart(2, "0");
@@ -60,5 +58,5 @@ const print = (train) => {
 };
 
 button.addEventListener("click", () => {
-    console.log(input);
-  });
+  getStation(input.value);
+});
