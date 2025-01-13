f74a9dc88ec4793bb5b78e05f6229b3bc1a1ef73
RobinChappalley <robinchappalley@gmail.com>
2025-01-13 12:52:19 +0100

    Ended Challenge

--- Challenge15/Deck.js
+++ Challenge15/Deck.js
@@ -1,10 +1,11 @@
-import Card from './Card.js'; 
+import Card from "./Card.js";
+import Shuffle from "./node_modules/lodash-es/shuffle.js";
 class Deck {
   #cards = [];
   constructor(options) {
     this.createFullDeck(options);
   }
-   createFullDeck = (options) =>  {
+  createFullDeck = (options) => {
     const suits = options.suits;
     const values = options.values;
     for (const suit of suits) {
@@ -13,15 +14,18 @@ class Deck {
         this.#cards.push(card);
       }
     }
+  };
+  get cards() {
+    return this.#cards;
   }
-    get cards() {
-        return this.#cards;
+  displayCards() {
+    for (const card of this.#cards) {
+      card.display();
     }
-    displayCards(){
-        for (const card of this.#cards) {
-            card.display();
-        }
-    }
+  }
+  shuffleCards() {
+    this.#cards = Shuffle(this.#cards);
+  }
 }
 
 export default Deck;
--- Challenge15/app.js
+++ Challenge15/app.js
@@ -1,10 +1,11 @@
 import Deck from './Deck.js';
 import Card from './Card.js';
+import shuffle from './node_modules/lodash-es/shuffle.js';
 
 const options = {
     suits: ['hearts', 'diamonds', 'clubs', 'spades'],
     values: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     };
 const deck = new Deck(options)
-console.log(deck.cards);
+deck.shuffleCards()
 deck.displayCards()
